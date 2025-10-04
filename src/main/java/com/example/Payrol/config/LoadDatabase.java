aa427623f196bbc722091cb0271f21901aba4464
Kirill <kiryusha.makarov.04@mail.ru>
2025-10-04 14:41:06 +0300

    Added conection to PostgreSQL

--- src/main/java/com/example/Payrol/config/LoadDatabase.java
+++ src/main/java/com/example/Payrol/config/LoadDatabase.java
@@ -3,32 +3,32 @@ package com.example.Payrol.config;
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.example.Payrol.entities.Employee;
-import com.example.Payrol.entities.Order;
-import com.example.Payrol.enums.Status;
-import com.example.Payrol.repositories.EmployeeRepository;
-import com.example.Payrol.repositories.OrderRepository;
+// import com.example.Payrol.entities.Employee;
+// import com.example.Payrol.entities.Order;
+// import com.example.Payrol.enums.Status;
+// import com.example.Payrol.repositories.EmployeeRepository;
+// import com.example.Payrol.repositories.OrderRepository;
 
-@Configuration
+// @Configuration
 public class LoadDatabase {
     
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
     
-    @Bean
-    CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository){
+    // @Bean
+    // CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository){
 
-        return args -> {
-            log.info("Preloading " + employeeRepository.save(new Employee("Kirill", "Makarov",  "Builder")));
-            log.info("Preloading " + employeeRepository.save(new Employee("Mark", "Zuckerberg", "Programmer")));
-            log.info("Preloading " + orderRepository.save(new Order("Some description", Status.COMPLETED)));
-            log.info("Preloading " + orderRepository.save(new Order("Some another description", Status.COMPLETED)));
-        };
+    //     return args -> {
+    //         log.info("Preloading " + employeeRepository.save(new Employee("Kirill", "Makarov",  "Builder")));
+    //         log.info("Preloading " + employeeRepository.save(new Employee("Mark", "Zuckerberg", "Programmer")));
+    //         log.info("Preloading " + orderRepository.save(new Order("Some description", Status.COMPLETED)));
+    //         log.info("Preloading " + orderRepository.save(new Order("Some another description", Status.COMPLETED)));
+    //     };
 
-    }
+    // }
     
 
 }
--- src/main/java/com/example/Payrol/repositories/OrderRepository.java
+++ src/main/java/com/example/Payrol/repositories/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.example.Payrol.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import com.example.Payrol.entities.Order;
